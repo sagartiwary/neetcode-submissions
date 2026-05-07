34a8ebb43b01ff14853c47df0bcc6efdc85e53e4
Sagar Tiwary <sagartiwary360@gmail.com>
2026-05-07 12:01:40 +0530

    Add: is-subsequence - submission-0

---
+++ Data Structures & Algorithms/is-subsequence/submission-0.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+
+
+    isSubsequence(s, t) {
+      if(s.length===0) return true;
+      let i=0;
+      let j=0;
+      while(i<s.length && j<t.length){
+        if(s[i]===t[j]){
+          i++
+        }
+        j++
+      }
+
+      return i===s.length
+    }
+}
