00f6fee43d4e3897b6809251b132188c4366d092
Sagar Tiwary <sagartiwary360@gmail.com>
2026-05-07 14:07:47 +0530

    Add: three-integer-sum - submission-1

---
+++ Data Structures & Algorithms/three-integer-sum/submission-1.js
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    threeSum(nums) {
+      let result=[];
+      nums.sort((a,b)=>a-b);
+      for(let i=0;i<nums.length;i++){
+        if(i>0 && nums[i]===nums[i-1]){
+          continue;
+        }
+        let j=i+1;
+        let k=nums.length-1;
+        while(j<k){
+          let sum=nums[i]+nums[j]+nums[k];
+          if(sum===0){
+              result.push([nums[i],nums[j],nums[k]]) 
+              j++   
+
+              while(nums[j]===nums[j-1] && j<k){
+                j++
+              }
+                   
+          } else if(sum<0){
+              j++
+          } else{
+            k--
+          }
+        }
+      }
+
+      return result;
+    }
+}
